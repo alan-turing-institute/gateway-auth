6877e5ba6151c0a484f00f93e4242bca50e9dd54
masonlr <masonlr@unimelb.edu.au>
2018-06-11 14:24:51 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+"""
+The main entry point for this flask app
+"""
+
+from json import load
+
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+# from connection import init_database, init_marshmallow
+# import connection.constants as const
+from routes import setup_routes
+
+app = Flask(__name__)
+
+# with open('config.json') as json:
+#     args = load(json)
+#     app.config['SQLALCHEMY_DATABASE_URI'] = args['database_url']
+#     const.JOB_MANAGER_URL = args['job_manager_url']
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# init_database(app)
+
+# init_marshmallow(app)
+
+api = Api(app)
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+setup_routes(app, api)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ routes/__init__.py
@@ -0,0 +1,27 @@
+"""
+Routes module
+"""
+
+# from connection.constants import StatusConverter
+# from .case_routes import CaseApi, CasesApi
+# from .fake_routes import TestData
+# from .job_routes import JobApi, JobsApi, StatusApi
+
+
+def setup_routes(app, api):
+    """
+    Set up the routes for these api end points
+    """
+
+    pass
+
+    # app.url_map.converters['stat'] = StatusConverter
+    #
+    # api.add_resource(CasesApi, '/case')
+    # api.add_resource(CaseApi, '/case/<int:case_id>')
+    # api.add_resource(JobsApi, '/job')
+    # api.add_resource(JobApi, '/job/<int:job_id>')
+    # api.add_resource(StatusApi, '/job/<int:job_id>/status')
+    #
+    # # Only while in development
+    # api.add_resource(TestData, '/test')
